adf5c982c187f3cb0c66c2e6967feeed31248af9
Derek Hurley <dzhurley@gmail.com>
2022-02-27 10:46:22 -0800

    dedupe search results

--- src/routes/search.js
+++ src/routes/search.js
@@ -1,6 +1,8 @@
 export async function get({ url }) {
   const resp = await fetch(
-    `https://api.spotify.com/v1/search?q=${url.searchParams.get('q')}&type=artist`,
+    `https://api.spotify.com/v1/search?q=${url.searchParams.get(
+      'q',
+    )}&type=artist`,
     {
       headers: {
         Authorization: `Bearer ${url.searchParams.get('token')}`,
@@ -9,6 +11,11 @@ export async function get({ url }) {
   ).then(r => r.json());
 
   return {
-    body: resp.artists.items,
+    body: resp.artists.items.reduce((artists, artist) => {
+      if (!artists.find(a => a.name === artist.name)) {
+        artists.push(artist);
+      }
+      return artists;
+    }, []),
   };
 }
